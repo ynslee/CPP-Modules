739b393554e0160ed53a97600635900df989bb44
chuchilini <vanesalees@gmail.com>
2023-11-08 16:18:49 +0200

    cpp00/ex00

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:09:36 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/11/07 16:18:31 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:18:18 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
@@ -21,9 +22,15 @@ int main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	i = 1;
-	while (i < argc)
+	for(i = 1; i < argc; i++)
 	{
-		
+		std::string str = argv[i];
+		for(std::string::iterator it = str.begin(); it != str.end(); ++it)
+		{
+    		*it = std::toupper(*it);
+		}
+		std::cout << str;
 	}
+	std::cout << std::endl;
+	return (0);
 }
